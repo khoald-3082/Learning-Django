c5392d343947db153872bd07b1be52b45b819b6b
khoald-3082 <lam.dang.khoa@sun-asterisk.com>
2025-08-01 13:26:00 +0900

    feat.versioning

--- real_world/real_world/settings.py
+++ real_world/real_world/settings.py
@@ -166,7 +166,11 @@ REST_FRAMEWORK = {
         'anon': '100/day',
         'user': '1000/day',
         'custom_view': '50/day',
-    }
+    },
+    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.URLPathVersioning',
+    'DEFAULT_VERSION': 'v1',
+    'ALLOWED_VERSIONS': ['v1'],
+    'VERSION_PARAM': 'version',
 }
 
 SIMPLE_JWT = {
